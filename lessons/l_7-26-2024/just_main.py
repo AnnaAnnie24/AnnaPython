fbadb729dc29c3c80fcf8b11ab82957aa997098c
AnnaAnnie24 <a.kyforuk@gmail.com>
2024-07-26 11:01:36 -0400

    was made for a lesson

--- lessons/l_7-26-2024/just_main.py
+++ lessons/l_7-26-2024/just_main.py
@@ -1,16 +1,14 @@
-flowers = []
+flowers = ["Lavender", "Rose", "Marigold", "Daisy", "Sunflower", "Dahlia", "Peony", "Lily", "Orchid", "Poppy"]
+
+# Print out each flower with long names (long name has 6 or more letters in it)
+
+for flower in flowers:
+    if len(flower) >= 6:
+        print(flower)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Print out each flower, which names is starting with 'D'
+Dee = "D"
+for flower in flowers:
+    if flower[0] == Dee:
+        print(flower)
