4474ca2fac846e3fd80179569b353ccb9a501191
AnnaAnnie24 <a.kyforuk@gmail.com>
2024-08-09 10:43:07 -0400

    Cool stuff

--- homework/home.py
+++ homework/home.py
@@ -10,3 +10,26 @@ Aaaa = "a"
 for woman_name in women_names:
     if woman_name[-1] == Aaaa:
         print("~", woman_name, "~")
+for woman_name in women_names:
+    number_of_letters = len(woman_name)
+    if number_of_letters == 5 or number_of_letters == 6:
+        print("*", woman_name, "*")
+
+Aaaa = "a"
+for woman_name in women_names:
+    number_of_letters = len(woman_name)
+    if woman_name[-1] == Aaaa and number_of_letters == 6:
+        print("~", woman_name, "~")
+
+IIIiii = "i"
+for woman_name in women_names:
+    number_of_letters = len(woman_name)
+    if woman_name[1] == IIIiii:
+        print("iii", woman_name, "iii")
+
+AAAAaa = "a"
+Aaa = "a"
+for woman_name in women_names:
+    number_of_letters = len(woman_name)
+    if woman_name == AAAAaa or woman_name == Aaa:
+        print("@", woman_name, "@")
---
+++ lessons/l_8-26-2024/text_analysis.py
@@ -0,0 +1,87 @@
+song_lyrics = """
+Oh, no
+See you walking 'round like it's a funeral
+Not so serious, girl, why those feet cold?
+We just getting started, don't you tiptoe
+Tiptoe, ah
+
+Waste time with a masterpiece
+Don't waste time with a masterpiece
+You should be rolling with me
+You should be rolling with me, ah
+
+You're a real-life fantasy
+You're a real-life fantasy
+But you're moving so carefully
+Let's start living dangerously
+
+Talk to me, baby
+I'm going blind from this sweet, sweet craving, whoa, oh
+Let's lose our minds and go fucking crazy
+I-I-I-I-I keep on hoping we'll eat cake by the ocean
+
+Walk for me, baby
+I'll be Diddy, you'll be Naomi, whoa, oh
+Let's lose our minds and go fucking crazy
+I-I-I-I-I keep on hoping we'll eat cake by the ocean
+
+Goddamn
+See you licking frosting from your own hands
+Want another taste, I'm begging: Yes, ma'am
+I'm tired of all this candy on the dry land
+Dry land, oh
+
+Waste time with a masterpiece
+Don't waste time with a masterpiece
+You should be rolling with me
+You should be rolling with me, ah
+
+You're a real-life fantasy
+You're a real-life fantasy
+But you're moving so carefully
+Let's start living dangerously (whoa)
+
+Talk to me, baby
+I'm going blind from this sweet, sweet craving, whoa, oh
+Let's lose our minds and go fucking crazy
+I-I-I-I-I keep on hoping we'll eat cake by the ocean
+
+Walk for me, baby (walk for me now)
+I'll be Diddy, you'll be Naomi, whoa, oh
+Let's lose our minds and go fucking crazy
+I-I-I-I-I keep on hoping we'll eat cake by the ocean (yeah, yeah)
+
+(Oh, ah) I-I-I-I-I keep on hoping we'll eat cake by the ocean
+(Oh, ah) I-I-I-I-I keep on hoping we'll eat cake by the ocean
+
+You're fucking delicious
+Talk to me, girl
+
+Talk to me, baby (talk to me, baby)
+I'm going blind from this sweet, sweet craving, whoa, oh
+Let's lose our minds and go fucking crazy
+I-I-I-I-I keep on hoping we'll eat cake by the ocean (hey!)
+
+Walk for me, baby (walk for me now)
+I'll be Diddy, you'll be Naomi, whoa, oh
+Let's lose our minds and go fucking crazy (that's right)
+I-I-I-I-I keep on hoping we'll eat cake by the ocean, hot damn
+
+Red velvet, vanilla, chocolate in my life
+Funfetti, I'm ready, I need it every night
+Red velvet, vanilla, chocolate in my life
+I-I-I-I-I keep on hoping we'll eat cake by the ocean
+"""
+
+# count how many letters 'a' are in this song text
+counter = 0
+A = "a"
+for symbol in song_lyrics:
+    if symbol == A:
+        counter = counter + 1
+        pass
+
+   # number_of_letters = len(song_line)
+   # if song_line == A and number_of_letters == 6:
+   #      print("~", woman_name, "~")
+print(counter)
