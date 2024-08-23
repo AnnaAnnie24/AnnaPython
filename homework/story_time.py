234d0a77db5ae9a588c71b699b69577e5bdafd7c
AnnaAnnie24 <a.kyforuk@gmail.com>
2024-08-23 10:38:22 -0400

    was created

---
+++ homework/story_time.py
@@ -0,0 +1,24 @@
+animal_story = """
+In the forest, where the trees grow tall.
+Lives a little bear, who goes by the name Teddy.
+This morning he decides to go berry picking, to make some compote for his birthday tomorrow.
+While picking berries he meets his friend, he is a tall and big giraffe, his name is Jerry.
+Jerry asks to pick berries with Teddy, but Teddy says "You are too tall to reach the bushes"
+Jerry looks at Teddy very sadly and walks away.
+Teddy feels sad and guilty of making Jerry feel bad.
+The next day he decides to say sorry to Jerry.
+So he walks up to his big door, carrying a small present for him, and knocks.
+But no answer, Teddy thought that Jerry might not be at home.
+He sees his other friend Robert the rabbit and asks him if he knows where Jerry is.
+Robert shakes his head and continues hopping along the stone path.
+Teddy passes the lake and asks Freddy and Freya the fish. 
+But, they also shake there heads and swim away.
+Teddy starts to cry and says that he has made his friend disappear.
+Maya the monkey who was listening to him the whole time tells him that that's not true .
+Felix the fox who is sly and clever, tells him to put on a mask to see a magic trick.
+At first Teddy is confused but does as Felix says and covers his face with a mask.
+When Teddy takes it off he sees all his friends holding gifts and balloons, 
+Wearing party hats and yelling Happy Birthday!!!
+Teddy sees Jerry and instantly hugs him he missed him so much and was so worried.
+                       THE END!!!
+"""
