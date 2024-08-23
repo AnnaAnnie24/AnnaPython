f47c3fe2511dc0d49daaade19fd5f8c3ebee89a2
AnnaAnnie24 <a.kyforuk@gmail.com>
2024-08-23 10:44:43 -0400

    created

---
+++ homework/my_ice_cream_and_soda_shop/menu.py
@@ -0,0 +1,18 @@
+ice_cream_dict = {
+    "Vanilla": 1.5,
+    "Chocolate": 1.5,
+    "Strawberry": 1.5,
+    "Mint Chocolate-Chip": 2.0,
+    "Cookies And Cream": 2.0,
+    "Cookie Dough": 2.0,
+    "Mint": 1.8,
+    "Cherry": 1.9,
+}
+soda_dict = {
+    "Sprite": 1.0,
+    "Coca-Cola": 1.0,
+    "Fanta": 1.0,
+    "Pepsi": 1.0,
+    "Mountain Dew": 1.0,
+    "Ginger Ale": 1.0,
+}
