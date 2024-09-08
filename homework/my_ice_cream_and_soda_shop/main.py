d5cff80277944887357fa29d59f381bc81de2860
AnnaAnnie24 <a.kyforuk@gmail.com>
2024-09-08 08:49:27 -0400

    Changes by Maksym

--- homework/my_ice_cream_and_soda_shop/main.py
+++ homework/my_ice_cream_and_soda_shop/main.py
@@ -1,28 +1,11 @@
-# Ice Cream
-ice_cream_dict = {
-    "Vanilla": 1.5,
-    "Chocolate": 1.5,
-    "Strawberry": 1.5,
-    "Mint Chocolate-Chip": 2.0,
-    "Cookies And Cream": 2.0,
-    "Cookie Dough": 2.0,
-    "Mint": 1.8,
-    "Cherry": 1.9,
-}
-soda_dict = {
-    "Sprite": 1.0,
-    "Coca-Cola": 1.0,
-    "Fanta": 1.0,
-    "Pepsi": 1.0,
-    "Mountain Dew": 1.0,
-    "Ginger Ale": 1.0,
-}
-print("Welcome To Annie's Ice-Cream And Soda Shop!!")
-number = 1
-for name, price in ice_cream_dict.items():
-    print(f"Option {number}: {name} – ${price}")
-    number += 1
+from source import (print_soda_menu,
+                    print_ice_cream_menu,
+                    get_ice_cream_price,
+                    get_soda_price,)
 
+print("Welcome To Annie's Ice-Cream And Soda Shop!!")
+
+print_ice_cream_menu()
 ice_cream = input("What Ice-Cream Would You Like?? ")
 ice_cream_price = get_ice_cream_price(ice_cream)
 
@@ -30,7 +13,6 @@ print_soda_menu()
 soda = input("What Soda Would You Like?? ")
 soda_price = get_soda_price(soda)
 
-# We need a price for the selected ice cream
-# We need a price for the selected soda
+total = ice_cream_price + soda_price
 
-print("Your total will be: ", )
+print("Your total will be:", total)
