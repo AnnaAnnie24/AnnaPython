4b6e5f2792ba42ba06654566bf79e1ab973fde7f
AnnaAnnie24 <a.kyforuk@gmail.com>
2024-09-13 11:38:42 -0400

    Changed files

--- homework/clothes_shop/clothes_shop_clothes.py
+++ homework/clothes_shop/clothes_shop_clothes.py
@@ -1,12 +1,26 @@
 from clothes_source import (print_clothes_list,
-                            get_clothes_price)
+                            get_clothes_price,
+                            display_clothes)
 
 print("Welcome To Annie's Clothes Shop!!")
 
 print_clothes_list()
-clothes = input("What Piece Of Clothing Would You Like?? ")
-clothes_price = get_clothes_price(clothes)
+total = 0
+while True:
+    clothes = input("What Piece Of Clothing Would You Like?? ")
+    clothes = int(clothes)
+    if clothes > 8 or clothes < 1:
+        print("Try Again Please...")
+        clothes = input("What Piece Of Clothing Do You Want?? ")
+    else:
+        clothes_price = get_clothes_price(clothes)
+        total = total + clothes_price
+        display_clothes(clothes)
+        answer = input("Do you want to continue shopping?? ")
+        if answer == "Yes":
+            continue
+        else:
+            break
 
-total = clothes_price
 
 print("Your total will be:", total)
--- homework/clothes_shop/clothes_source.py
+++ homework/clothes_shop/clothes_source.py
@@ -1,9 +1,22 @@
 from stuff import clothes_dict
+
+
 def print_clothes_list():
     number = 1
     for name, price in clothes_dict.items():
         print(f"Option {number}: {name} – ${price}")
         number += 1
+
+
+def display_clothes(option_number):
+    count = 1
+    for name, price in clothes_dict.items():
+        if option_number == count:
+            print("You have purchased a", name)
+            break
+        else:
+            count += 1
+
 
 def get_clothes_price(clothes):
     number = int(clothes)
